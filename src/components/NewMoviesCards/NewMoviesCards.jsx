2d966947b73c4402384ab66989f269a81f21b903
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-07-26 17:34:47 -0300

    Update/Añadiendo imagen y texto de cada pelicula a las cards

--- src/components/NewMoviesCards/NewMoviesCards.jsx
+++ src/components/NewMoviesCards/NewMoviesCards.jsx
@@ -3,66 +3,46 @@ import { Link } from 'react-router-dom';
 import Loading from '../Loading';
 
 //import './NewMovesCards.sass';
+const { Meta } = Card;
 
 const NewMovesCards = props => {
     const {title, popularMovies} = props;
-    console.log(props);
-    // console.log(popularMovies.result.results.length);
-    // console.log(popularMovies.result.results);
-
+  
     //Destructuración de los resultados de newMovies.result, de las películas;
     const { results } = popularMovies.result;
-    console.log(results);
+    console.log(results.poster_path); 
 
     return (
         <div>
-             {results.map(movie => (                 
-                
-                <Card title="Card title">
-                Card content
-                </Card>               
-                
-                
+             {results.map(movie => (  
+                <Card 
+                    hoverable
+                    style={{ width: 240 }}                       
+                    cover={<CardPopular movie={movie}/>}
+                >
+                    <Meta title={movie.title} 
+                    description={<i className="far fa-eye"></i>} />
+                </Card>  
             ))}
-           {/*  <List
-                className="movie-list"
-                size="default"
-                header={<h2>{title}</h2>}
-                bordered
-                dataSource={popularMovies.result.results}
-                renderItem={movie => <RenderMovie movie={movie}/>}
-            /> */}
-
         </div>
     );
 };
 
-// const RenderMovie = props => {
-//     const { movie: 
-//         {
-//             poster_path,
-//             id,
-//             title
-//         } 
-//     } = props;
-//     const posterPath = `https://image.tmdb.org/t/p/original${poster_path}`;
+const CardPopular = props => {
+    const { movie: 
+        {
+            poster_path,
+            id,
+            title
+        } 
+    } = props;
+    const posterPath = `https://image.tmdb.org/t/p/original${poster_path}`;
+    console.log(posterPath);
+    return (
 
-//     return (
-//         <List.Item className="movie-list__movie">
-//             <List.Item.Meta 
-//                 avatar={<Avatar src={posterPath} />}
-//                 title={ <Link to={`/movie/${id}`}>{title}</Link>}
-//             />
-//             <Link to={`/movie/${id}`}>
-//                 <Button 
-//                     type="primary"
-//                     shape="circle"
-//                 >
-
-//                 </Button>
-//             </Link>
-//         </List.Item>
-//     )
-// };
+            <img src={posterPath} alt=""/>
+       
+    )
+};
 
 export default NewMovesCards;
