47d218889966865ea36f0a9f0d46ebcd09965660
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-07-27 20:15:19 -0300

    Update/Añadiendo funcionalidad al componente de peliculas populares

--- src/pages/popular.jsx
+++ src/pages/popular.jsx
@@ -1,5 +1,22 @@
+import {useEffect} from 'react';
+import useFetch from '../hooks/useFetch';
+import { URL_API, API_KEY } from '../utils/constants';
+import PopularMoviesCards from '../components/PopularMoviesCards/';
+import Paginador from '../components/Paginador';
+
 const Popular = () => {
-    return "Estamos en popular"
+    
+    const url = `${URL_API}/movie/popular?api_key=${API_KEY}&language=en-ES&page=1`        
+
+    const popularMovies = useFetch(url);
+    console.log(popularMovies);
+    
+    return (
+        <div>
+            <PopularMoviesCards popularMovies={popularMovies} />   
+        </div>   
+        
+    )
 };
 
 export default Popular;
