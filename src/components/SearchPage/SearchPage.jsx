fc459ea07b10602ef5b5e1597915e81059eacc20
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-07-30 21:07:33 -0300

    Update/Haciendo fetch cada que se hace un keyup en el input

--- src/components/SearchPage/SearchPage.jsx
+++ src/components/SearchPage/SearchPage.jsx
@@ -1,45 +1,105 @@
+import {useState, useEffect} from 'react';
 import { Input, List, Avatar, Button, Card, } from 'antd';
 import { Link } from 'react-router-dom';
 import Loading from '../Loading';
+import { URL_API, API_KEY } from '../../utils/constants';
 import './SearchPage.sass';
+import useFetch from '../../hooks/useFetch';
 
-const SearchPage = props => {
-    let busqueda 
-    const {title, allMovies} = props;
 
-    if(allMovies.loading || !allMovies.result){
-        return <Loading />
-    }
+const SearchPage = () => {
+    let leticia
+    // const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=Jack+Reacher`
+    const [inputUsuario, setImputUsuario] = useState('')
+    const [url, setUrl] = useState([]);
+   // let busquedaUsuario 
+    //const {title, allMovies} = props;
+
+    // if(allMovies.loading || !allMovies.result){
+    //     return <Loading />
+    // }
   
-    const { results } = allMovies.result;
-    console.log(results); 
+    // const { results } = allMovies.result;
+    // console.log(results); 
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(
+                `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${inputUsuario}`
+            );
+            const movies = await response.json()
+            setUrl(movies.results)
+            console.log(movies);
+            console.log(movies.results);
+            // const letciana = movies.results
+            // console.log(letciana);
+
+            // const {page, results, total_pages, total_results} = movies;
+            // console.log(total_results);
+
+        })();
+    }, [inputUsuario]);
+   
+    // const {page, results, total_pages, total_results} = movies;
+    // console.log(total_results);
+/*     useEffect(() => {
+        (async () => {
+          const response = await fetch(
+            `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${inputUsuario}`
+          );
+          const movies = await response.json();
+          console.log(movies);
+          //setLoadingState(false);
+          //setUrl(movies);
+        })();
+      }, [page, inputValue]); */
+
     const onChange = (e) => {
-        console.log(e.target.value);
-        busqueda = e.target.value
+        setImputUsuario(e.target.value)
+        console.log(setImputUsuario);
+        //console.log(e.target.value);
+        //busquedaUsuario = e.target.value
+        //setBusqueda(busquedaUsuario)
     }
+    
 
     return (
         <div className="search-page">
             <h1>Buscador</h1>
             <Input placeholder="Escribe el nombre de la película que quieras buscar" onChange={onChange}/>
             <div className="card-new-movie">
-            {results.map(movie => (  
-                <Prueba movie={movie}/>
-                
-            ))}
+            {/* {url.results.map(movie => (  
+                <Prueba movie={movie}/>                
+                ))} */}
+            <Prueba url={url}/>
+                {/* {letciana.map(movie => (  
+                    <Prueba movie={movie}/>
+                    
+                ))} */}
             </div>
         </div>
     );
 };
 
 const Prueba = props => {
-    const { movie: 
+    /* const { movie: 
         {
             poster_path,
             id,
             title
         } 
-    } = props;
+    } = props; */
+    const 
+        {
+            poster_path,
+            id,
+            title
+        }
+     = props
+     console.log(props);
+
+    /*  props.map(movie => (  
+        console.log(movie)          
+        )) */
     const posterPath = `https://image.tmdb.org/t/p/original${poster_path}`;
     console.log(title);
 
--- src/pages/search.jsx
+++ src/pages/search.jsx
@@ -1,15 +1,24 @@
 import SearchPage from '../components/SearchPage';
+import {useState, useEffect} from 'react'
 import useFetch from '../hooks/useFetch';
 import { URL_API, API_KEY } from '../utils/constants';
 
+
 const Search = () => {
-    const url = `${URL_API}/movie/now_playing?api_key=${API_KEY}&language=en-ES&page=1`
+    // const url = `${URL_API}/movie/now_playing?api_key=${API_KEY}&language=en-ES&page=1`
+    const [busqueda, setBusqueda] = useState('undefined');
+
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${busqueda}`
 
     const allMovies = useFetch(url);
+    
+    useEffect(() => {
+        console.log('dadadad');
+    }, [busqueda, setBusqueda]);
 
     console.log(allMovies);
     return(
-        <SearchPage allMovies={allMovies}/>
+        <SearchPage allMovies={allMovies} busqueda={busqueda} setBusqueda={setBusqueda} url={url}/>
     )
 };
 
