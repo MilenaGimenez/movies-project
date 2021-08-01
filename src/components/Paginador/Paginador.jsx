7ebdb05c7b9463c258c2d7872e3df39cbfe9a573
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-08-01 09:17:54 -0300

    Update/Accediendo a onchange del paginador

--- src/components/Paginador/Paginador.jsx
+++ src/components/Paginador/Paginador.jsx
@@ -1,35 +1,99 @@
 import {Pagination} from 'antd';
 import {URL_API, API_KEY} from '../../utils/constants'
 import useFetch from '../../hooks/useFetch';
+import {useState, useEffect} from 'react';
 
 const Paginador = (props) => {
-    let pageNumber = 1
-    const url = `${URL_API}/movie/now_playing?api_key=${API_KEY}&language=en-ES&page=${pageNumber}`
-    const {title, popularMovies} = props;
-    //Destructuración de los resultados de newMovies.result, de las películas;
-    const { result } = popularMovies
-    const { results: {
-        total_pages,
-        total_restsults,
-        page
-    } } = popularMovies.result;
+    // const [paginator, setPaginator] = useState({
+    //     offset: 0,
+    //     currentPageElements: [],
+    //     elementsPerPage: 20,
+    //     pagesCount: 1,
+    //     allElements: [],
+    //     totalElementsCount: 0
+    // })
 
-    console.log(result);
-    console.log(result.total_results);
-    const onChange = () => {
+    /* const getAllElements = async () => {
+        const url = `${URL_API}/movie/now_playing?api_key=${API_KEY}&language=en-ES&page=1`
+        const allElements = await fetch(url);
+        const movies = await allElements.json()
+        //console.log(allElements);
+        //console.log(movies);
+        setPaginator({
+            allElements: movies.results,
+            totalElementsCount: movies.total_results
+        }, () => {
+            console.log('hello');
+            //setPaginationStates();
+        });
+    } */
+    //let pageNumber = 1
+    //const url = `${URL_API}/movie/now_playing?api_key=${API_KEY}&language=en-ES&page=${pageNumber}`
+    //const {title, newMovies} = props;
+    //Destructuración de los resultados de newMovies.result, de las películas;
+    //const { results } = newMovies.result;
+    //const { result } = newMovies
+    // const { results: {
+    //     total_pages,
+    //     total_restsults,
+    //     page
+    // } } = newMovies.result;
+
+    /* const onChange = () => {
         pageNumber = pageNumber + 1
         console.log(pageNumber);
-        // useFetch(url)
+    } */
+    const {namePage} = props
+    console.log(namePage);
+
+    const [url, setUrl] = useState([]);
+    const [pageSelect, setPageSelect] = useState('1')
+    useEffect(() => {
+        (async () => {
+            const response = await fetch(
+                `${URL_API}/movie/${namePage}?api_key=${API_KEY}&language=en-ES&page=${pageSelect}`
+            );
+            const movies = await response.json()
+            setUrl(movies)
+            setPageSelect(movies.page)
+            console.log(movies);
+            console.log(url);
+            console.log('pageselct en l fetch', pageSelect);
+            //console.log(movies);
+            //console.log(movies.results);
+
+        })();
+    }, [setPageSelect]);
+    const { results, total_pages, total_results } = url;
+    console.log(total_pages);
+
+    //const {title, newMovies} = url;
+    //Destructuración de los resultados de newMovies.result, de las películas;
+    //const { page, results, total_pages, total_results } = url.result;
+
+    //console.log(newMovies);
+
+    const onChange = (e) => {
+        //pageNumber = pageNumber + 1
+        //console.log('on change');
+        console.log(e);
+        console.log('antes del set, soy page select', pageSelect);
+        setPageSelect(e)
+        console.log('despues del set, soy page select', pageSelect);
+        // page = e
+        // console.log(page);
+        // function(page, pageSize)
     }
+    
     return (
         <div>
             <Pagination 
-                onChange={onChange}
                 defaultCurrent={1}  
-                total={result.total_results}
-                showTotal={total => `Total ${result.total_results} items`}  
-                  
-
+                onChange={onChange}
+                total={total_results}
+                showTotal={total => `Total ${total} items`}  
+                //pageSize={total_pages}
+                //showSizeChanger={true}
                 />
         </div>
     );
--- src/pages/new-movies.jsx
+++ src/pages/new-movies.jsx
@@ -4,34 +4,20 @@ import { URL_API, API_KEY } from '../utils/constants';
 import NewMoviesCards from '../components/NewMoviesCards/';
 import Paginador from '../components/Paginador';
 
-const NewMovies = () => {
-    
+const NewMovies = () => {    
     const url = `${URL_API}/movie/now_playing?api_key=${API_KEY}&language=en-ES&page=1`
 
-    // const myInit = {
-    //     method: 'GET',
-    //     mode: 'cors',
-    //     cache: 'default'
-    // };
-    
+    const namePage = 'now_playing';
 
     const newMovies = useFetch(url);
-    console.log(newMovies);
-    
-
-    // useEffect(() => {
-    //     const url = `${URL_API}/movie/now_playing?api_key=${API_KEY}&language=en-ES&page=1`
-    //     const newMovies = useFetch(url);
-    //     console.log(newMovies);
-    // }, [<NewMoviesCards />]);
+    console.log(newMovies);    
     
     return (
         <div>
             <NewMoviesCards newMovies={newMovies} />   
-            {/* <Paginador newMovies={newMovies}
-            url={url}/> */}
-        </div>   
-        
+            <Paginador namePage={namePage}
+            />
+        </div>           
     )
 };
 
