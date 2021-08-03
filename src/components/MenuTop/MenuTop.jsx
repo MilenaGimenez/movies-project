1b242b0525d8e24b14b7b8bd11010a99e118e761
MilenaGimenez <milenanicolegimenez@gmail.com>
2021-08-03 09:02:25 -0300

    Update/Añadiendo estilos al navbar y a las listas de peliculas

--- src/components/MenuTop/MenuTop.jsx
+++ src/components/MenuTop/MenuTop.jsx
@@ -7,21 +7,20 @@ import "./MenuTop.sass";
 const MenuTop = () => {
     return (
         <div className="menu-top">
-            <div className="menu-top_logo">
-                {/* <Logo /> */} Logo 🍿
+            <div className="menu-top__logo">
+                <div className="logo2"></div>
             </div>
             <Menu
                 theme="dark"
                 mode="horizontal"
                 defaultSelectedKeys={["1"]}
-                style={{lineHeight: "64px"}}
+                style={{lineHeight: "78px"}}
             >
                 <Menu.Item key="1">
                     <Link to="/">Home</Link>
                 </Menu.Item>
                 <Menu.Item key="2">
                     <Link to="/new-movies">Últimos lanzamientos</Link>
-                    {/* <Link to={{}}>New Movies</Link> */}
                 </Menu.Item>
                 <Menu.Item key="3">
                     <Link to="/popular">Más populares</Link>
